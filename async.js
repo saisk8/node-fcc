e067859a117a191714a009ccbd5e9c2311e369a7
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-04-04 19:30:09 +0530

    Do asynchronus file acess

---
+++ async.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+function afterRead(error, data) {
+  if (!error) {
+    console.log(data.toString().split('\n').length - 1);
+  }
+  else {
+    console.error("Error occured");
+  }
+}
+
+fs.readFile(process.argv[2], afterRead)
